7cf23df2eaf8a75dbd1c3359bc8a3584dfcb233c
Anggi Ridho <anggi.ridho@gmail.com>
2016-04-15 15:05:41 +0700

    setting default auth and auth for admin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use App\User;
+use App\Admin;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,14 +16,24 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
         
-        DB::table('users')->delete();
+        DB::table('users')->truncate();
         $users = [
-            ['name' => 'superadmin', 'email' => 'superadmin@test.com', 'password' => Hash::make('admin')],
-            ['name' => 'administrator', 'email' => 'administrator@test.com', 'password' => Hash::make('admin')],
+            ['name' => 'customer', 'email' => 'customer@test.com', 'password' => Hash::make('admin')],
+            ['name' => 'member', 'email' => 'member@test.com', 'password' => Hash::make('admin')],
         ];
             
         foreach ($users as $user) {
             User::create($user);
+        }
+
+        DB::table('admins')->truncate();
+        $admins = [
+            ['name' => 'superadmin', 'email' => 'superadmin@test.com', 'password' => Hash::make('admin')],
+            ['name' => 'administrator', 'email' => 'administrator@test.com', 'password' => Hash::make('admin')],
+        ];
+            
+        foreach ($admins as $admin) {
+            Admin::create($admin);
         }
 
         Model::reguard();
